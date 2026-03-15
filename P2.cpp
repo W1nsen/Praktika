f7e26344ad54de8e0094555d84f381577b5db2ff
W1nsen <liwinsen@gmail.com>
2026-03-15 19:44:41 +0100

    p2 meter fertig

--- P2.cpp
+++ P2.cpp
@@ -6,10 +6,17 @@ int main()
     cout << "Bitte geben Sie die Temperatur in Grad Celsius ein: ? ";
     double temp; cin >> temp;
 
-    temp = (temp * 1.8) +32;
+    temp = (temp * 1.8) + 32;
 
     cout << "Die Temperatur in Fahrenheit lautet: " << temp;
 
     cout << endl;
-    return 0;
+
+
+    cout << "Bitte geben Sie die Laenge in Metern ein: ? ";
+    double meter; cin >> meter;
+
+    meter *= 3.2808;
+
+    cout << "Die Laenge in Fuss lautet: " << meter << endl;
 }
