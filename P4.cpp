cc1546adaefae2060f78f1b0394590cebb0cd0d2
W1nsen <liwinsen@gmail.com>
2026-03-21 09:00:36 +0100

    fertig mit P4

--- P4.cpp
+++ P4.cpp
@@ -2,52 +2,51 @@
 using namespace std;
 
 int main(){
-    // cout << "Text Input: ";
-    // string text; getline(cin,text);
-    // cout << endl << "Verschiebungsposition: ";
-    // int v; cin >> v;
+    cout << "Text Input: ";
+    string text; getline(cin,text);
+    cout << endl << "Verschiebungsposition: ";
+    int v; cin >> v;
 
-    // //check auf buchstabe gross und kliein
-    // // nach z soll a
+    //check auf buchstabe gross und kliein
+    // nach z soll a
 
-    // string newText = "";
+    string newText = "";
 
-    // while(v > 26)
-    // {
-    //     v -= 26;
-    // }
+    while(v > 26)
+    {
+        v -= 26;
+    }
 
-    // for(int i = 0; i < text.length();i++)
-    // {
-    //     //check auf buchstabe
-    //     if (int(text[i]) <= int('z') && int(text[i]) >= int('a'))
-    //     {
-    //         if(int(text[i]) + v >= int('z'))
-    //         {
-    //             text[i] = int(text[i]) - 26 + v;
+    for(int i = 0; i < text.length();i++)
+    {
+        //check auf buchstabe
+        if (int(text[i]) <= int('z') && int(text[i]) >= int('a'))
+        {
+            if(int(text[i]) + v >= int('z'))
+            {
+                text[i] = int(text[i]) - 26 + v;
                 
 
-    //         }else{
-    //             text[i] = int(text[i]) + v;
-    //         }
-    //     }
+            }else{
+                text[i] = int(text[i]) + v;
+            }
+        }
 
-    //     if (int(text[i]) <= int('Z') && int(text[i]) >= int('A'))
-    //     {
-    //         if (int(text[i]) + v >= int('Z'))
-    //         {
-    //             text[i] = int(text[i]) - 26 + v;
-    //         }
-    //         else
-    //         {
-    //             text[i] = int(text[i]) + v;
-    //         }
-    //     }
-    // }
+        if (int(text[i]) <= int('Z') && int(text[i]) >= int('A'))
+        {
+            if (int(text[i]) + v >= int('Z'))
+            {
+                text[i] = int(text[i]) - 26 + v;
+            }
+            else
+            {
+                text[i] = int(text[i]) + v;
+            }
+        }
+    }
 
-    // cout << text;
+    cout << text;
 
-    bool checkStunde,checkMinute;
     int stunde,minute,takt;
     do
     {
@@ -99,4 +98,5 @@ int main(){
         
         
     };
+
 }
